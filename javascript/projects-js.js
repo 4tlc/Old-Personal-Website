c4549d7e7675de0ae818264f7d05ffaa3f8a0657
Timothy Cronin <tcroniniv@gmail.com>
2022-05-12 20:58:24 -0400

    add fishy feeder and orginization

--- javascript/projects-js.js
+++ javascript/projects-js.js
@@ -1,3 +1,7 @@
 document.querySelector('.metFinLogo').addEventListener('click', (e) => {
     location.href = 'metFinPage.html'
 });
+
+document.querySelector('.ffLogo').addEventListener('click', (e) => {
+    location.href = 'fishyFeederPage.html'
+});
